98518a21784eb4630c74da206484172ae6bcaf08
Sindre Rotnes Erichsen <sindreerichsen@gmail.com>
2023-04-15 15:37:53 +0200

    endret env navn

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,4 +1,5 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit';
 import { env } from '$env/dynamic/public';
+import { NEXT_PUBLIC_SUPABASE_ANON_KEY, NEXT_PUBLIC_SUPABASE_URL } from '$env/static/private';
 
-export const supabaseClient = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
+export const supabaseClient = createClient(NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_ANON_KEY);
